6511772d3f312b6301a82d3a2928a48634da300c
ValeriyaMix <lerka-mishka@yandex.ru>
2022-07-28 17:26:32 +0300

    Database and tables "Products", "Categories" and "Products_Categories" were added.

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿Select P.Name, C.Name
+from Products P
+Left join Products_Categories PC
+on P.Id = PC.ProductId
+Left join Categories C
+on PC.CategoryId = C.Id
---
+++ dbo.Products_Categories.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products_Categories] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [ProductId]  INT NOT NULL,
+    [CategoryId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Products_Categories_Products] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([Id]),
+    CONSTRAINT [FK_Products_Categories_Categories] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([Id])
+);
+
